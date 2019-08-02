619b2e446bd1d27ae5ad93206991f2fedb779e73
Ramiro <ramiro.portas@gmail.com>
2019-08-02 15:49:04 -0300

    se actualiza, server se agrega configuracion json

--- socketServer/server.js
+++ socketServer/server.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var config = require('./config/config');
+
 const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
@@ -7,17 +9,17 @@ const http = require('http')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
-const port = 3000
+const port = config.api.port
 
 //corriendo el servidor
 server.listen(port, () => {
-	console.log(`Server running in http://localhost:${port}`);
-	io.on('connection', function(socket) {
-		//imprimiendo el id del cliente conectado 
-		console.log(`client: ${socket.id}`);
-		socket.on("init", function(data) {
-			console.log(data);
-		});
-	});
+    console.log(`Server running in http://localhost:${port}`);
+    io.on('connection', function(socket) {
+        //imprimiendo el id del cliente conectado 
+        console.log(`client: ${socket.id}`);
+        socket.on("init", function(data) {
+            console.log(data);
+        });
+    });
 
 })
