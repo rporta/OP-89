90d2047cad2caccb44f079309f0f001dab61d7c3
Your Name <you@example.com>
2019-08-05 01:48:36 -0300

    se sigue desarrollando menu

--- socketClientProcess/src/pages/rightPanel.vue
+++ socketClientProcess/src/pages/rightPanel.vue
@@ -13,10 +13,10 @@
       </f7-list-item>
       <f7-list-item  accordion-item title="Lista de clientes process">
         <f7-icon :text-color="((clients.length > 0 && Process) ? true : false)  ? 'green' : 'red'" slot="media" ios="f7:phonelink" aurora="f7:phonelink" md="material:phonelink"></f7-icon>
-        <f7-accordion-content :style="generateColor('rgb(11, 11, 11)')" >
+        <f7-accordion-content :style="generateColor('rgb(17, 17, 17)')" >
           <f7-list-item accordion-item v-show="client.type == 'Process' " :title="client.driver.os + ' : #' +client.id.slice(0, 5)" v-for="client in clients" :key="client.id">
             <f7-icon text-color="green" slot="media" ios="f7:laptop" aurora="f7:laptop" md="material:laptop"></f7-icon>
-            <f7-accordion-content bg-color="black">
+            <f7-accordion-content :style="generateColor('rgb(10, 10, 10)')">
               <f7-list-item link="#" @click="redirectTo('/infoClient/')" title="Ver informacion">
                 <f7-icon text-color="green" slot="media" ios="f7:layers_alt_fill" aurora="f7:layers_alt_fill" md="material:layers_alt_fill"></f7-icon>
               </f7-list-item>
@@ -40,10 +40,10 @@
       </f7-list-item>
       <f7-list-item  accordion-item title="Lista de clientes wap" >
         <f7-icon :text-color="((clients.length > 0 && Wap) ? true : false) ? 'green' : 'red'" slot="media" ios="f7:phone_android" aurora="f7:phone_android" md="material:phone_android"></f7-icon>
-        <f7-accordion-content :style="generateColor('rgb(11, 11, 11)')" >
+        <f7-accordion-content :style="generateColor('rgb(17, 17, 17)')" >
           <f7-list-item accordion-item v-show="client.type == 'Wap' " :title="client.driver.os + ' : #' +client.id.slice(0, 5)" v-for="client in clients" :key="client.id">
             <f7-icon text-color="green" slot="media" ios="f7:phone_android" aurora="f7:phone_android" md="material:phone_android"></f7-icon>
-            <f7-accordion-content bg-color="black">
+            <f7-accordion-content :style="generateColor('rgb(10, 10, 10)')">
               <f7-list-item link="#" @click="redirectTo('/infoClient/')" title="Ver informacion">
                 <f7-icon text-color="green" slot="media" ios="f7:layers_alt_fill" aurora="f7:layers_alt_fill" md="material:layers_alt_fill"></f7-icon>
               </f7-list-item>
