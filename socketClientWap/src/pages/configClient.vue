b6333fe6975764d8d4950ab796a8e2793567997c
Ramiro <ramiro.portas@gmail.com>
2019-08-05 16:23:08 -0300

    se genera plantilla de configuracion para client (remote)

--- socketClientWap/src/pages/configClient.vue
+++ socketClientWap/src/pages/configClient.vue
@@ -5,10 +5,50 @@
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
       </f7-nav-right>
     </f7-navbar>
-    <f7-block strong>
-      <p># {{socketId}} </p>
-      <p>Requested content not found.</p>
-    </f7-block>
+    <f7-block-title>Config socket </f7-block-title>
+    <f7-list no-hairlines-md form form-store-data ref="configSocket">
+      <f7-list-input
+        label="Port"
+        floating-label
+        type="text"
+        name="port"
+        placeholder="Your Port"
+        clear-button
+        :value="config.api.port" 
+        @input="config.api.port = $event.target.value"        
+      >
+                      <f7-icon text-color="lightblue" slot="media" ios="f7:settings_appl" aurora="f7:settings_appl" md="material:settings_appl"></f7-icon>
+      </f7-list-input>
+      <f7-list-input
+        label="Host"
+        floating-label
+        type="text"
+        name="host"
+        placeholder="Your URL Host"
+        clear-button
+        :value="config.api.host" 
+        @input="config.api.host = $event.target.value"   
+      >
+                      <f7-icon text-color="lightblue" slot="media" ios="f7:settings_appl" aurora="f7:settings_appl" md="material:settings_appl"></f7-icon>
+      </f7-list-input>      
+    </f7-list>
+    <f7-block-title>Socket </f7-block-title>
+    <f7-block>
+      <f7-row>
+        <f7-col>
+          <f7-button fill color="red">disconnect</f7-button>
+        </f7-col>        
+      </f7-row>      
+    </f7-block>  
+
+    <f7-block-title>Set default config </f7-block-title>
+    <f7-block>
+      <f7-row>
+        <f7-col>
+          <f7-button @click="resetDefaultConfig()" fill color="red">reset</f7-button>
+        </f7-col>       
+      </f7-row>      
+    </f7-block>  
   </f7-page>
 </template>
 <script>
@@ -28,6 +68,25 @@
         required : false,
         default: "",
       }
-    },    
+    }, 
+    methods: {
+      resetDefaultConfig(){
+        var formConfigPerfil = this.$refs.configSocket.$el;
+        var getDataForm = this.$f7.form.convertToData(formConfigPerfil);
+        const configDefaultString = JSON.stringify(this.configDefault);
+        const configDefaultJSON = JSON.parse(configDefaultString); 
+        this.$f7.form.fillFromData(formConfigPerfil, configDefaultJSON.perfil);
+        this.config = configDefaultJSON;
+      }
+    },
+    mounted() {
+      this.$f7ready((f7) => {
+        // Init cordova APIs (see cordova-app.js)
+        if (f7.device.cordova) {
+          cordovaApp.init(f7);
+        }
+        // Set Dom7 style, events
+      }); 
+    }
   };
 </script>
