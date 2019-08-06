e7e29b0f2a4ee688711afb4f6506362433d20ea1
Your Name <you@example.com>
2019-08-05 21:37:13 -0300

    se desarrolla informacion client

--- socketClientProcess/src/pages/configApp.vue
+++ socketClientProcess/src/pages/configApp.vue
@@ -6,6 +6,15 @@
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
       </f7-nav-right>
     </f7-navbar>
+    <f7-block-title>General : </f7-block-title>
+    <f7-list>
+      <f7-list-item :title="'Id : # ' + socketId">
+        <f7-icon text-color="deeporange" slot="media" ios="f7:add" aurora="f7:add" md="material:add"></f7-icon>
+      </f7-list-item>
+      <f7-list-item :title="'Type : ' + config.type">
+        <f7-icon text-color="deeporange" slot="media" ios="f7:add" aurora="f7:add" md="material:add"></f7-icon>
+      </f7-list-item>
+    </f7-list>
     <f7-block-title>Config socket</f7-block-title>
     <f7-list no-hairlines-md form form-store-data ref="configSocket">
       <f7-list-input
@@ -65,7 +74,8 @@
         config : config,
         configDefault : configDefault,
         socketTitle: "Socket Offline",
-        socketColor: "red"
+        socketColor: "red",
+        socketId: ""
       };
     },
     methods: {
@@ -96,6 +106,7 @@
         // Set data 
         this.socketTitle = socket.connected ? "Socket Online" : "Socket Offline";
         this.socketColor = socket.connected ? "green" : "red";
+        this.socketId = socket.id;
 
         var self = this;
 
--- socketClientWap/src/pages/infoClient.vue
+++ socketClientWap/src/pages/infoClient.vue
@@ -1,13 +1,11 @@
 <template>
   <f7-page name="infoClient">
-    <f7-navbar :title="'informacion Client :' + ' # ' + socketId" back-link="Back">
+    <f7-navbar title="Informacion Cliente " back-link="Back">
       <f7-nav-right>
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
       </f7-nav-right>
     </f7-navbar>
-    <f7-block inset>
-      <p>General : </p>
-    </f7-block>
+    <f7-block-title>General : </f7-block-title>
     <f7-list>
       <f7-list-item :title="'Id : #' + infoClient.id">
         <f7-icon text-color="deeporange" slot="media" ios="f7:add" aurora="f7:add" md="material:add"></f7-icon>
@@ -16,9 +14,7 @@
         <f7-icon text-color="deeporange" slot="media" ios="f7:add" aurora="f7:add" md="material:add"></f7-icon>
       </f7-list-item>
     </f7-list>
-    <f7-block inset>
-      <p>Driver : </p>
-    </f7-block>
+    <f7-block-title>Driver : </f7-block-title>
     <f7-list>
       <f7-list-item :title="index.charAt(0).toUpperCase() + index.slice(1) + ' : ' + driver" v-for="(driver, index) in infoClient.driver">
         <f7-icon text-color="deeporange" slot="media" ios="f7:add" aurora="f7:add" md="material:add"></f7-icon>
