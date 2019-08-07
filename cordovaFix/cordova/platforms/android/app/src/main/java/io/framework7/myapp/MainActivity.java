f4da16db02a2847fd940723bfa06e88cefd041a4
Ramiro <ramiro.portas@gmail.com>
2019-08-07 10:46:38 -0300

    actualiza MainActivity.java, se agregan methods

--- cordovaFix/cordova/platforms/android/app/src/main/java/io/framework7/myapp/MainActivity.java
+++ cordovaFix/cordova/platforms/android/app/src/main/java/io/framework7/myapp/MainActivity.java
@@ -22,8 +22,29 @@ package io.framework7.myapp;
 import android.os.Bundle;
 import org.apache.cordova.*;
 
+import org.json.JSONObject;
+
+import java.lang.*;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import com.emulate.ProcessKey;
+import com.socketImplement.socketConection;
+
 public class MainActivity extends CordovaActivity
 {
+    public static String TAG = "MainActivity";
+    public String URL = "";
+    public List URLList = new ArrayList();
+    public String PageStatus = "";
+    public boolean startFinishLoadPag = false;
+    public String dataFW;
+    public Integer resolveCase;
+    public String dimensionFw;
+    public socketConection socket;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -36,6 +57,209 @@ public class MainActivity extends CordovaActivity
         }
 
         // Set by <content src="index.html" /> in config.xml
-        loadUrl(launchUrl);
+        loadUrl(launchUrl);//aca ejecuta CordovaActivity.init(),
+        //CordovaActivity.init() ejecuta makeWebView() y al final CordovaWebViewImpl.ini(cordovaInterface, pluginEntries, preferences)
+        //makeWebView() retorna la instancia de CordovaWebViewImpl() que la almacena en CordovaActivity.appView
+        //para construir la instancia de CordovaWebViewImpl(), se pasa como argumento la instancia de CordovaWebViewImpl.createEngine(), es decir
+        //CordovaWebViewImpl(CordovaWebViewImpl.createEngine(context, ...))
+        //
+        //a donde va el contexto de CordovaActivity?
+        //
+        //el method CordovaWebViewImpl.createEngine(), le pasa el contexto de CordovaActivity  y retorna una instancia de SystemWebViewEngine,
+        //el constructor de la clase SystemWebViewEngine, en base al contexto 'CordovaActivity' crea la instancia de SystemWebView,
+        //SystemWebViewEngine realiza pasajes de parametros entre sus constructores, y finalmente en SystemWebViewEngine.webView se almacena la instancia de SystemWebView
+        //al crear la instancia de SystemWebView, en el constructor, le pasa el conteto al super WebView y ejecuta el contructor de WebView
+        //WebView extiende de MockView, al ejecutar el constructor de WebView, le pasa el conteto al super MockView (aca creo que es posible realizar recursion CordovaActivity <-> MockView),
+        //la clase MockView viene del package
+
+//        socketConection s = new socketConection("hera.opratel.com", 10001);
+
+//        this.setSocket(s);
+//        this.getSocket().init();
     }
+
+
+    public socketConection getSocket() {
+        return socket;
+    }
+
+    public void setSocket(socketConection socket) {
+        this.socket = socket;
+    }
+
+
+    /*
+     * RAMIRO PORTAS
+     * ESTE METHOD SE LLAMA CUANDO SE DISPARA EL EVENTO AL CARGAR UNA NUEVA PAGINA
+     * SE LLEGA A ESTE METHOD REALIZANDO Override DE CordovaActivity.onPageFinishedLoading()
+     * EN CordovaWebViewImpl SE CREA EL CAMPO contextCordovaActivity
+     * CUANDO SE EJECUTA CordovaWebViewImpl.init() - (@Override) se instancia CordovaWebViewImpl.EngineClient()
+     * EN EL MISMO METHOD luego de la instancia CordovaWebViewImpl.EngineClient(), se ejecuta CordovaWebViewImpl.EngineClient.addContextCordovaActivity()
+     * Y LE PASA EL CONTEXTO CordovaActivity.
+     *
+     * LOS CAMBIOS REALIZADOS SON
+     * +CordovaWebViewImpl.EngineClient.addContextCordovaActivity() (NUEVO)
+     * +CordovaWebViewImpl.EngineClient.contextCordovaActivity:CordovaActivity (NUEVO)
+     * +CordovaWebViewImpl.contextCordovaActivity:CordovaActivity (NUEVO)
+     * +CordovaWebViewImpl.CordovaWebViewImpl(CordovaActivity context, CordovaWebViewEngine cordovaWebViewEngine) (SE MODIFICO)
+     * +CordovaActivity.makeWebView() (SE MODIFICO), //aca recien se pasa el contexto CordovaActivity
+     * +CordovaActivity.onPageFinishedLoading() (NUEVO)
+     * +MainActivity.onPageFinishedLoading() (NUEVO)
+     *
+     *
+     * */
+
+    @Override
+    public void onPageFinishedLoading(String url) {
+        String nameofCurrMethod = new Throwable()
+                .getStackTrace()[0]
+                .getMethodName();
+
+        this.URL = url;
+        if(this.startFinishLoadPag == false){
+            //finalizo la carga url local, iniciamos por primera vez, aun no inicia el flujo web
+
+            Integer w = this.appView.getView().getWidth();
+            Integer h = this.appView.getView().getHeight();
+            LOG.d(TAG, ", appView getWidth : " + w + ", appView getHeight : " + h);
+
+            this.URLList.add(url);
+            this.startFinishLoadPag = true;
+            this.PageStatus = "Inicializamos";
+        }else {
+            if(url.indexOf("file") != -1){
+                //finalizo la carga url local, realizamos inyection javascript (flag), aun no inicia el flujo web
+                if (this.PageStatus == "volvemos por segunda vez"){
+                    String json = "{'mensaje' : '"+ this.PageStatus +"' }";
+                    String js = "javascript:java.send(" + json + ")";
+                    this.appView.loadUrl(js);
+                }
+            }else{
+                //finalizo la carga url remota
+                this.URLList.add(url);
+                if(this.PageStatus == "Inicializamos"){
+                    //finalizo la carga url remota por primera vez, realizamos captura de URL, realizamos captura, volvemos a cargar el recurso local
+                    this.PageStatus = "volvemos por segunda vez";
+
+
+                    //creo un delay, para para lanzar la captura
+                    TimerTask task = new TimerTask() {
+                        public void run() {
+                            cordovaInterface.pluginManager.exec("Screenshot", "saveScreenshot", "", "[\"jpg\",50,\"opraTestScreenShot\"]");
+                            loadUrl((String) URLList.get(0));
+                        }
+                    };
+                    long delay = 1000L;
+                    Timer timer = new Timer("Screenshot");
+                    timer.schedule(task, delay);
+
+                    task = null;
+                    timer = null;
+
+                }else if (this.PageStatus == "volvemos por segunda vez"){
+                    //finalizo la carga url remota por segunda vez, realizamos casos (CASO 1, CASO2), realizamos captura, volvemos a cargar el recurso local
+                    if(this.resolveCase == 1){
+                        //caso 1
+                        try{
+
+                            JSONObject JsonDataFW = (new JSONObject(dataFW));
+                            //preparo caso 1
+                            Integer x = JsonDataFW.getInt("x");
+                            Integer y = JsonDataFW.getInt("y");
+                            x *= 2;
+                            y *= 2;
+                            String ParamFocus = "{\"top\":0,\"left\":0,\"right\":" + x + ",\"bottom\":"+ y +"}";
+                            //realizamos toch
+                            cordovaInterface.pluginManager.exec("Focus", "focus", "", "[" + ParamFocus + "]");
+
+                            //creo un delay, para para lanzar la captura
+                            TimerTask task = new TimerTask() {
+                                public void run() {
+                                    cordovaInterface.pluginManager.exec("Screenshot", "saveScreenshot", "", "[\"jpg\",50,\"opraTestScreenShot\"]");
+                                    loadUrl((String) URLList.get(0));
+                                }
+                            };
+                            long delay = 1000L;
+                            Timer timer = new Timer("Screenshot");
+                            timer.schedule(task, delay);
+
+                            task = null;
+                            timer = null;
+
+                        }catch (Exception e){
+
+                        }
+                    }else if (this.resolveCase == 2){
+                        //caso 2
+                        try{
+
+                            JSONObject JsonDataFW = (new JSONObject(dataFW));
+                            //preparo caso 2
+                            Integer x = JsonDataFW.getInt("x");
+                            Integer y = JsonDataFW.getInt("y");
+                            String text = JsonDataFW.getString("text");
+
+
+                            //creo un delay, para para lanzar la captura
+                            TimerTask task = new TimerTask() {
+                                public void run() {
+                                    cordovaInterface.pluginManager.exec("Screenshot", "saveScreenshot", "", "[\"jpg\",50,\"opraTestScreenShot\"]");
+                                    loadUrl((String) URLList.get(0));
+                                }
+                            };
+                            long delay = 1000L;
+                            Timer timer = new Timer("Screenshot");
+                            timer.schedule(task, delay);
+
+                            task = null;
+                            timer = null;
+
+                        }catch (Exception e){
+
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    /*
+     * RAMIRO PORTAS
+     * ESTE METHOD SE INVOCA CUANDO FLUJO WEB ENVIA DATA AL MODULO APP(JAVA)
+     * */
+
+    @Override
+    public void onDataFW(String dataFW){
+        String nameofCurrMethod = new Throwable()
+                .getStackTrace()[0]
+                .getMethodName();
+        this.dataFW = dataFW;
+
+        if(dataFW.indexOf("height") != -1){
+            LOG.d(TAG, nameofCurrMethod + ", dataFW dimensiones: " + dataFW );
+            this.dimensionFw = dataFW;
+        }
+
+
+        if(URL.indexOf("file") == -1){
+            //Aca el recurso local me esta enviando la data del socket
+
+            if(this.PageStatus == "volvemos por segunda vez"){
+
+                if(dataFW.indexOf("text") != -1){
+                    //caso 2 touch y data para cargar en input: coordenadas(x, y ), data
+                    LOG.d(TAG, nameofCurrMethod + ", dataFW caso 2: " + dataFW );
+                    this.resolveCase = 2;
+
+                }else{
+                    //caso 1 touch : url, coordenadas(x, y )
+
+                    LOG.d(TAG, nameofCurrMethod + ", dataFW caso 1: " + dataFW );
+                    this.resolveCase = 1;
+                }
+                loadUrl((String) URLList.get(this.URLList.size() - 1));//aca siempre cargamos la ultima URL, remota
+            }
+        }
+    }
+
 }
