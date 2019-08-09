e74efce918d019326f156d28fb96eacd12567709
rporta <you@example.com>
2019-08-09 10:31:20 -0300

    se apaga socket del lado de f7, se prende de java

--- socketClientWap/src/js/socket.js
+++ socketClientWap/src/js/socket.js
@@ -1,4 +1,5 @@
 import io from './libs/socket.io-2.2.0.js';
 import config from '../config/config.json';
 
-socket = io("http://" + config.api.host + ":" + config.api.port);
+// socket = io("http://" + config.api.host + ":" + config.api.port);
+socket = io();
